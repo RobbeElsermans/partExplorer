26292c0a5b9c86879a70bec2301043831377fcfe
Robbe Elsermans <robels@outlook.com>
2022-08-30 13:12:20 +0200

    Add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="columns">
+      <div class="column">
+        <p>blabla</p>
+      </div>
+      <div class="column">
+        <table class="table">
+          <tr>
+            <th>NAME</th>
+            <th>VALUE</th>
+            <th>TYPE</th>
+            <th>PACKAGE</th>
+            <th>QUANTITY</th>
+            <th>WEBSITE</th>
+            <th>COMMENT</th>
+            <th>FOOTPRINT</th>
+            <th>DATASHEET</th>
+          </tr>
+          <tr v-for="(item, i) in database" v-bind:key="i">
+            <th v-for="(cell, i) in item" v-bind:key="i">{{ cell }}</th>
+          </tr>
+        </table>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  /* eslint-disable */
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      database: [
+        ["M3", "M3", "M3", "M3", "M3", "M3", "M3", "M3", "M3"],
+        ["M5", "M5", "M5", "M5", "M5", "M5", "M5", "M5", "M5"],
+        ["M8", "M8", "M8", "M8", "M8", "M8", "M8", "M8", "M8"]
+      ]
+    }
+  },
+  methods: {
+    getData(id){
+      if(id == -1){
+        //Get all data
+
+      }
+      else
+      {
+        //get specific data
+        
+      }
+    }
   }
 }
 </script>
+
+<style>
+tr {
+  border-width: 2px;
+  border-color: black;
+  border-style: solid;
+}
+</style>
